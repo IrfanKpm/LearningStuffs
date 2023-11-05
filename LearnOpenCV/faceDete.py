097d1e850a4ce556acaf2afe8f8c2311ccf59ff9
MUHAMMED IRFAN P <112098500+IrfanKpm@users.noreply.github.com>
2023-11-05 20:59:42 +0530

    Update faceDete.py

--- LearnOpenCV/faceDete.py
+++ LearnOpenCV/faceDete.py
@@ -1,13 +1,7 @@
 import cv2 as cv
-import matplotlib.pyplot as plt
-
-# video = sequence of image
-# Frame Per Secont (FPS) : number of image per secont 
 
 face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-
 cap = cv.VideoCapture(0)
-
 while True:
     status,frame = cap.read()
     frame = cv.flip(frame, 1)
@@ -17,9 +11,9 @@ while True:
         # print(faces) [x,y,w,h] of face
         for x,y,w,h in faces:
          cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
-
         cv.imshow("Video",frame)
         if cv.waitKey(1) == ord("q"):
           break
     else:
         break
+cv.destroyAllWindows()
