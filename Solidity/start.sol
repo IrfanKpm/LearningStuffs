894225c9d5f009fea9fabd63aace04cb4aeeb34b
MUHAMMED IRFAN P <112098500+IrfanKpm@users.noreply.github.com>
2023-12-27 17:45:18 +0530

    Create start.sol

---
+++ Solidity/start.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.22;
+
+contract Hello{
+    // comments here
+
+    string public welcome = "Hello World";
+    bool status = true;
+    uint8 MAX_SUPPLY = 10;
+    uint256 WAIT_TIME = 1 days;
+    
+
+}
